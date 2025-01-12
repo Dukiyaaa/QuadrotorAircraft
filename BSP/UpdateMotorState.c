fd11fc9f24ba2c467ee866ce3b95c7de672a4ee8
Dukiya <Dukiya@qq.com>
2025-01-13 01:06:57 +0800

    将代码从gitee迁移到github

--- BSP/UpdateMotorState.c
+++ BSP/UpdateMotorState.c
@@ -9,7 +9,7 @@ void UpdateMotorState(void) {
 		// 从接收机获取映射值 (0 ~ 1.0)
 			float mappedValue = Receiver_GetMappedValue(i);
 
-			// 将映射值转化为占空比 (0.05 ~ 1.0)
+			// 将映射值转化为占空比 (0.05 ~ 0.10)
 			float motorDuty = 0.05 * mappedValue + 0.05; // 映射到占空比范围
 //      printf("Roll = %.2f\tPitch = %.2f\n",Receiver_GetMappedAngle(CHANNEL4_INDEX),Receiver_GetMappedAngle(CHANNEL3_INDEX));
 			// 更新电机状态
